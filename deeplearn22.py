d2fd93bb777571f58e6db526149146aa3539d628
tkn5100 <chyoshich3585@gmail.com>
2022-11-05 23:00:35 +0900

    added images

--- deeplearn22.py
+++ deeplearn22.py
@@ -5,7 +5,7 @@ def sigmoid(x):
     f = 1 / (1 + np.exp(-x))
     return f
 
-img = cv2.imread(str("mario.jpg"))
+img = cv2.imread(str("0.png"))
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 h,s,v = cv2.split(hsv)
 
